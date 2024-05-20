vimpostor/vim-lumen

commit 145a2fd683add0488c067f20ca8458cc4660974a

Escape all system command filenames
In the macos implementation it is possible that spaces appear in the
absolute file path, so we have to escape it for all system command uses.

All other platform implementations are not able to end up with spaces
in system command arguments (most of them are hardcoded and do not
depend on the directory where vim is installed).

Fixes #13

autoload/lumen/platforms/macos.vim
@@ -1,4 +1,4 @@
-let s:exe = resolve(expand('<sfile>:p:h')) . '/macos/watcher'
+let s:exe = shellescape(resolve(expand('<sfile>:p:h')) . '/macos/watcher')
 
 func lumen#platforms#macos#watch_cmd()
 	return [s:exe]

commit 863cb1908eebf1ebe9e483cd5e54ea8aaaf752c6

macos: Escape filename properly
shellescape() is not quite correct, as it also adds single quotes around
otherwise proper filenames, e.g. it turns test into 'test', which
jobstart() will complain about in neovim.

The regression was introduced in
145a2fd683add0488c067f20ca8458cc4660974a to fix the problem with spaces
in the filename in #13.
Because vim's job_start() implementation is a little more robust than
the neovim counterpart and can handle the single quotes, noone noticed
the problem back then.

To fix this we switch to fnameescape(), which doesn't enclose everything
in quotes but rather escapes every problematic character individually,
e.g. test turns into test, and test asdf turns into test\ asdf.

Fixes #14

autoload/lumen/platforms/macos.vim
@@ -1,4 +1,4 @@
-let s:exe = shellescape(resolve(expand('<sfile>:p:h')) . '/macos/watcher')
+let s:exe = fnameescape(resolve(expand('<sfile>:p:h')) . '/macos/watcher')
 
 func lumen#platforms#macos#watch_cmd()
 	return [s:exe]